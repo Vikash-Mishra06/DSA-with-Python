7caf43bc6631f2b38ee8345b5a0b1605a0adb714
Vikash Mishra <mishravikash0612@gmail.com>
2025-12-30 21:23:19 +0530

    Practiced 5 Questions: 1) Positive or Negative number, 2) Even or Odd number, 3) Sum of First N Natural numbers, 4) Sum of N natural numbers, 5) Sum of numbers in a given range.

---
+++ Day2/index.py
@@ -0,0 +1,85 @@
+# Q.1) Positive or Negative number.
+
+    # def check_number(num):
+    #     # Check if number is greater than 0
+    #     if num > 0:
+    #         return "Positive"
+        
+    #     # Check if number is less than 0
+    #     elif num < 0:
+    #         return "Negative"
+        
+    #     # If not greater and not less, it must be zero
+    #     else:
+    #         return "Zero"
+
+    # print(check_number(5))    # Positive
+    # print(check_number(-3))   # Negative
+    # print(check_number(0))    # Zero
+
+# ----------------------------------------------------------------------------
+
+# Q.2) Even or Odd number.
+
+    # def sum_of_n_numbers(n):
+    #     total = 0  # this will store the sum
+        
+    #     # loop from 1 to n (inclusive)
+    #     for i in range(1, n + 1):
+    #         total += i   # add each number to total
+        
+    #     return total
+
+    # print(sum_of_n_numbers(5))   # 15
+    # print(sum_of_n_numbers(3))   # 6
+    # print(sum_of_n_numbers(1))   # 1
+
+# ----------------------------------------------------------------------------
+
+# Q.3) Sum of First N Natural numbers.
+
+    # def sum_of_n_numbers(n):
+    #     total = 0  # this will store the sum
+        
+    #     # loop from 1 to n (inclusive)
+    #     for i in range(1, n + 1):
+    #         total += i   # add each number to total
+        
+    #     return total
+
+    # print(sum_of_n_numbers(5))   # 15
+    # print(sum_of_n_numbers(3))   # 6
+    # print(sum_of_n_numbers(1))   # 1
+
+# ----------------------------------------------------------------------------
+
+# Q.4) Sum of N natural numbers.
+
+    # def sum_of_n(n):
+    #     total = 0   # start sum at 0
+
+    #     # add every number from 1 to n
+    #     for i in range(1, n + 1):
+    #         total += i
+
+    #     return total
+
+    # print(sum_of_n(5))   # 15
+    # print(sum_of_n(3))   # 6
+    # print(sum_of_n(1))   # 1
+
+# ----------------------------------------------------------------------------
+
+# Q.5) Sum of numbers in a given range.
+
+    # def sum_in_range(L, R):
+    #     total = 0  # to store the sum
+
+    #     # loop from L to R (inclusive)
+    #     for num in range(L, R + 1):
+    #         total += num  # add each number to total
+
+    #     return total
+
+    # print(sum_in_range(1, 5))   # 15
+    # print(sum_in_range(3, 7))   # 25
